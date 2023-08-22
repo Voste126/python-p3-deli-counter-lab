d43baa80b4726f31bc58cfdb4a68c9182ebc466f
Voste126 <steveaustine126@gmail.com>
2023-08-22 10:33:06 +0300

    my commit

---
+++ lib/deli_counter.py
@@ -0,0 +1,24 @@
+katz_deli = []
+
+def line(katz_deli):
+    if not katz_deli:
+        line_message = "The line is currently empty."
+    else:
+        line_message = "The line is currently:"
+        for index, customer in enumerate(katz_deli, start=1):
+            line_message += f" {index}. {customer}"
+    
+    return line_message
+
+def take_a_number(katz_deli, new_customer):
+    katz_deli.append(new_customer)
+    position = len(katz_deli)
+    print(f"Welcome, {new_customer}. You are number {position} in line.")
+
+def now_serving(katz_deli):
+    if not katz_deli:
+        print("There is nobody waiting to be served!")
+        return
+    
+    serving_customer = katz_deli.pop(0)
+    print(f"Currently serving {serving_customer}.")
